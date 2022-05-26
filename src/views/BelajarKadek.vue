2f892851f357e2a71d1f42275a252b380fd853b1
madeserius <imadeagus0209@gmail.com>
2022-05-26 18:43:17 +0700

    cara membuat setInterval

--- src/views/BelajarKadek.vue
+++ src/views/BelajarKadek.vue
@@ -1,13 +1,39 @@
 <template>
-  <div>
-    <h3>Belajar Serius</h3>
+  <div id="vueApp">
+    <h3>Belajar Serius {{ subject }}</h3>
+    <p>number : {{ number }}</p>
+    <p>kadek : {{ kadek }}</p>
+    <p>kiki : {{ kiki }}</p>
+    <p>fadel : {{ fadel }}</p>
   </div>
 </template>
 
 <script>
+// vue.createApp(sample).mount("#vueApp");
 export default {
-  setup() {
-    return {};
+  name: "belajar kadek",
+  data() {
+    const subject = "";
+    return {
+      subject: "vuejs",
+      kadek: "++++++++++",
+      kiki: "++",
+      fadel: "++++",
+      number: 0,
+    };
+  },
+  mounted() {
+    setInterval(() => {
+      this.number++;
+      this.kadek = this.kadek + "+";
+    }, 1000);
+
+    setInterval(() => {
+      this.kiki = this.kiki + "+";
+    }, 1500);
+    setInterval(() => {
+      this.fadel = this.fadel + "+";
+    }, 700);
   },
 };
 </script>
