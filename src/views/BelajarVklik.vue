bab5360491643d02ca5b01d4fd85f8811463885c
madeserius <imadeagus0209@gmail.com>
2022-05-30 23:31:14 +0700

    penjumlahan

--- src/views/BelajarVklik.vue
+++ src/views/BelajarVklik.vue
@@ -1,8 +1,9 @@
 <template>
   <div>
+    <input type="text" v-model="subject" />
     <h3>Belajar {{ subject }}</h3>
     <button
-      @click="makeItfun()"
+      v-on:click="makeItfun()"
       class="
         bg-blue-500
         hover:bg-blue-700
@@ -15,20 +16,98 @@
     >
       Make it fun
     </button>
+    <button
+      v-on:click="check()"
+      class="
+        bg-blue-500
+        hover:bg-blue-700
+        text-white
+        font-bold
+        py-2
+        px-4
+        rounded-full
+      "
+    >
+      Check Subject
+    </button>
+    <button
+      v-on:click="hapus()"
+      class="
+        bg-red-500
+        hover:bg-blue-700
+        text-white
+        font-bold
+        py-2
+        px-4
+        rounded-full
+      "
+    >
+      hapus
+    </button>
+    <button
+      v-on:click="fajri()"
+      class="
+        bg-pink-900
+        hover:bg-blue-700
+        text-white
+        font-bold
+        py-2
+        px-4
+        rounded-full
+      "
+    >
+      fajri
+    </button>
+    <button
+      v-on:click="fadel()"
+      class="
+        bg-green-500
+        hover:bg-red-500
+        text-white
+        font-bold
+        py-2
+        px-4
+        rounded-full
+      "
+    >
+      fadel
+    </button>
+    <p>nilai {{ score }}</p>
   </div>
 </template>
 
 <script>
 export default {
-  subject: "Vuehs",
+  // subject: "VueJs",
   data() {
     return {
-      subject: "Vuejs",
+      subject: "VueJs",
+      score: 0,
     };
   },
   methods: {
+    fadel() {
+      this.subject = "fadel";
+      this.score = this.score + 20;
+    },
+    fajri() {
+      this.subject = "fajri";
+      this.score = this.score + 90 / 10;
+    },
+    hapus() {
+      this.subject = "";
+      this.score = 0;
+    },
     makeItfun() {
       this.subject = this.subject + "!!!!";
+    },
+    check() {
+      console.log("cek masuk");
+      if (this.subject === "fajri") {
+        this.score = 100;
+      } else {
+        this.score = -50;
+      }
     },
   },
 };
