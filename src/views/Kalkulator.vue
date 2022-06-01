373aec62e55b7874a52366f6f5643351f39e18ab
madeserius <imadeagus0209@gmail.com>
2022-06-01 19:57:57 +0700

    belajar false & true

--- src/views/Kalkulator.vue
+++ src/views/Kalkulator.vue
@@ -1,113 +1,61 @@
 <template>
-  <p>input 1:</p>
-  <div><input type="text" v-model="input1" /></div>
-  <br />
-  <p>input 2:</p>
-  <div><input type="text" v-model="input2" /></div>
-  <button
-    v-on:click="hapus()"
-    class="
-      bg-red-500
-      hover:bg-blue-700
-      text-white
-      font-bold
-      py-2
-      px-4
-      rounded-full
-    "
-  >
-    C
-  </button>
-  <button
-    v-on:click="kurang()"
-    class="
-      bg-yellow-500
-      hover:bg-blue-700
-      text-white
-      font-bold
-      py-2
-      px-4
-      rounded-full
-    "
-  >
-    -
-  </button>
-  <button
-    v-on:click="kali()"
-    class="
-      bg-green-500
-      hover:bg-red-500
-      text-white
-      font-bold
-      py-2
-      px-4
-      rounded-full
-    "
-  >
-    x
-  </button>
-  <button
-    v-on:click="bagi()"
-    class="
-      bg-blue-500
-      hover:bg-red-500
-      text-white
-      font-bold
-      py-2
-      px-4
-      rounded-full
-    "
-  >
-    /
-  </button>
-  <button
-    v-on:click="tambah()"
-    class="
-      bg-blue-500
-      hover:bg-red-500
-      text-white
-      font-bold
-      py-2
-      px-4
-      rounded-full
-    "
-  >
-    +
-  </button>
-  <div>
-    <p>Hasil : {{ hasil }}</p>
+  <div class="container">
+    <h3>Belajar Vue.Js</h3>
+    <ol start="12" v-if="statusJangan">
+      <li v-for="subject in subjects" v-bind:key="subject.title">
+        {{ subject.title }}-type-{{ subject.type }}
+      </li>
+    </ol>
+    <div v-else>
+      <p>ok</p>
+    </div>
+    <button
+      v-on:click="changeStatus()"
+      class="
+        bg-blue-500
+        hover:bg-blue-700
+        text-white
+        font-bold
+        py-2
+        px-4
+        rounded-full
+      "
+    >
+      Check Subject
+    </button>
   </div>
 </template>
+
 
 <script>
 export default {
   data() {
     return {
-      input1: 0,
-      input2: 0,
-      hasil: 0,
+      subjects: [
+        { title: "JavaScript", type: "Native" },
+        { title: "Vue", type: "Framework" },
+        { title: "React", type: "Framework" },
+      ],
+      statusJangan: false,
     };
   },
   methods: {
-    tambah() {
-      this.hasil = parseInt(parseInt(this.input1) + parseInt(this.input2));
-    },
-    bagi() {
-      this.hasil = this.input1 / this.input2;
-    },
-    kali() {
-      this.hasil = this.input1 * this.input2;
-    },
-    kurang() {
-      this.hasil = this.input1 - this.input2;
-    },
-    hapus() {
-      this.input1 = "";
-      this.input2 = "";
+    changeStatus() {
+      this.statusJangan = !this.statusJangan;
     },
   },
 };
 </script>
 
-<style lang="scss" scoped>
+<style>
+ol,
+ul {
+  list-style-type: decimal;
+  list-style-position: inside;
+  -webkit-margin-before: 1em;
+  -webkit-margin-after: 1em;
+  -webkit-margin-start: 0px;
+  -webkit-margin-end: 0px;
+  -webkit-padding-start: 0px;
+}
 </style>
